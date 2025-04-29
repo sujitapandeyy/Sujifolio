c387c9635726c39a51f70abba32e433fbcd6c06a
sujitapandeyy <sujitapandey806@gmail.com>
2025-04-30 00:47:24 +0545

    simple update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
+  output: 'export',
+  basePath: '/Sujifolio',
+  assetPrefix: '/Sujifolio/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
